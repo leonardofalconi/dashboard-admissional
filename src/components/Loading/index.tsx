9fcaada6743f1a721df7d00c98e9e145642c6824
Leonardo Falconi <leonardofalconi@Leonardos-Mac-mini.local>
2024-11-04 07:22:01 -0300

    feat: create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { IconLoading } from '~/libs/Icons/loading'
+
+import * as Styled from './styles'
+
+export const Loading = () => (
+  <Styled.Container>
+    <IconLoading width="150px" height="150px" color="#FFF" />
+  </Styled.Container>
+)
